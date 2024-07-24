14859ec04e837c9b42f8a2c8aae7ff7db533da72
Sudeep1505 <95011726+Sudeep1505@users.noreply.github.com>
2024-07-24 23:13:32 +0530

    Add files via upload

---
+++ CompareNumbers.java
@@ -0,0 +1,17 @@
+package AnudipPract;
+public class CompareNumbers {
+	public static void main(String[] args) {
+
+		int numerator = 10;
+		int denominator = 20;
+
+		// Compare the values
+		if (numerator > denominator) {
+			System.out.println("Greater");
+		} else if (numerator == denominator) {
+			System.out.println("Equal");
+		} else {
+			System.out.println("Less");
+		}
+	}
+}
